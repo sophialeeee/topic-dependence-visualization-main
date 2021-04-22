95502111c62cbc5662c8727fe5e9acad7cc96097
Sophia.lee <583171726@qq.com>
2021-04-22 21:39:34 +0800

    jj

--- src/draw-map.ts
+++ src/draw-map.ts
@@ -400,13 +400,13 @@ export async function drawMap(
     }
     // 补全键名，键名是所有的topic_id
     communityRelation = completeObj(communityRelation);
-    //const radius = svg.clientHeight < svg.clientWidth ? svg.clientHeight / 2 - 24 : svg.clientWidth / 2 - 24;
-      const radius = svg.clientWidth / 2 ;
+    const radius = svg.clientHeight < svg.clientWidth ? svg.clientHeight / 2 - 24 : svg.clientWidth / 2 - 24;
+    const radiusx = svg.clientWidth / 2 ;
     console.log("communityRelation", communityRelation)
     console.log(Object.keys(communityRelation).length)
     // 处理只有一个簇的情况
     if (Object.keys(communityRelation).length === 0) {
-        const nodes0 = {r: radius, id: 0, cx: radius, cy: radius}
+        const nodes0 = {r: radius, id: 0, cx: radiusx, cy: radius}
         const edges0 = {}
         const sequence0 = [0]
         //globalSequence是簇之间的序列关系
@@ -423,7 +423,7 @@ export async function drawMap(
             .selectAll('circle')
             .append('circle')
             .attr('r', radius)
-            .attr('cx', radius)
+            .attr('cx', radiusx)
             .attr('cy', radius)
             .attr('id', d => 'com' + 0)
             .attr('fill', (d, i) => colors[i % colors.length][1]);
@@ -431,7 +431,7 @@ export async function drawMap(
         // 绘制簇内信息
 
         const tmp = calcCircleLayout(
-            {x: radius, y: radius},
+            {x: radiusx, y: radius},
             radius,
             graph[0],
             0
@@ -608,7 +608,7 @@ export async function drawMap(
     else {
 
         const {nodes, edges, sequence} = calcCircleLayout(
-            {x: radius, y: radius},
+            {x: radiusx, y: radius},
             radius,
             communityRelation,
             topicId2Community[-1] !== undefined ? topicId2Community[-1] : undefined
@@ -1187,7 +1187,7 @@ export async function drawMap(
             // 传入的是那个知识簇的id
             // 调用函数计算每个簇的圆心和半径
             const {nodes, edges} = calcCircleLayout(
-                {x: radius, y: radius},
+                {x: radiusx, y: radius},
                 radius,
                 communityRelation,
                 topicId2Community[-1] !== undefined ? topicId2Community[-1] : undefined
@@ -1219,7 +1219,7 @@ export async function drawMap(
                 .delay(300)
                 .attr('x', d => d.cx - 14 * judgementStringLengthWithChinese(topics[sequences[d.id][0]]) / 2)
                 .attr('y', (d, i) => {
-                    if (nodes[i].cy < radius) return d.cy - d.r - 24;
+                    if (nodes[i].cy < radiusx) return d.cy - d.r - 24;
                     return d.cy + d.r + 24;
                 })
                 .attr('font-size', 14)
@@ -1346,7 +1346,7 @@ export async function drawMap(
             // 传入的是那个知识簇的id
             // 调用函数计算每个簇的圆心和半径
             const {nodes, edges} = calcCircleLayoutWithoutReduceCrossing(
-                {x: radius, y: radius},
+                {x: radiusx, y: radius},
                 radius,
                 communityRelation,
                 globalSequence,
@@ -1501,7 +1501,7 @@ export async function drawMap(
         //@ts-ignore
         function comSecond(id) {
             const {nodes, edges} = calcCircleLayoutSecondLayer(
-                {x: radius, y: radius},
+                {x: radiusx, y: radius},
                 radius,
                 communityRelation,
                 globalSequence,
@@ -1793,7 +1793,7 @@ export async function drawMap(
         //@ts-ignore
         function nodeFirst(id, c) {
             const {nodes, edges} = calcCircleLayoutSecondLayer(
-                {x: radius, y: radius},
+                {x: radiusx, y: radius},
                 radius,
                 communityRelation,
                 globalSequence,
